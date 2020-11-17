10587a3e5ae93fac133490fb44357eb0fc5d524f
Daniel <danielwiktorczyk@gmail.com>
2020-11-17 14:08:29 -0500

    Add Main Driver
    Generate, Read, Solve. 
    
    Left to do: Analyze + Output

--- Input/puzzle_generator.py
+++ Input/puzzle_generator.py
@@ -2,29 +2,28 @@ import copy
 import os
 import random
 
-
-class PuzzleGenerator:
-
-    def __init__(self, row, column, count):
-        file_path = 'Input/Puzzles/Puzzles.txt'
-        if os.path.exists(file_path):
-            os.remove(file_path)
-        f = open(file_path, "x")
-        f = open(file_path, "a")
-        values = row * column
-        possible_tiles = []
-
-        for i in range(values):
-            possible_tiles.append(i)
-
-        for i in range(count):
-            puzzle_tiles = copy.deepcopy(possible_tiles)
-            random.shuffle(puzzle_tiles)
-            for j in range(len(puzzle_tiles)):
-                f.write(str(puzzle_tiles[j]) + " ")
-            f.write("\n")
-
-        f.close()
+file_path = "C:\Git Repositories\COMP472-A2\Input\Puzzles\Puzzles.txt"  # TODO shorten path
 
 
-PuzzleGenerator(2, 4, 50)
+def generate_puzzles(row, column, count):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    f = open(file_path, "x")
+    f = open(file_path, "a")
+    values = row * column
+    possible_tiles = []
+
+    for i in range(values):
+        possible_tiles.append(i)
+
+    for i in range(count):
+        puzzle_tiles = copy.deepcopy(possible_tiles)
+        random.shuffle(puzzle_tiles)
+        for j in range(len(puzzle_tiles)):
+            f.write(str(puzzle_tiles[j]) + " ")
+        f.write("\n")
+
+    f.close()
+
+
+# generate_puzzles(2, 4, 50)

5a815455cfdca364ccffc48d43735733f5cc5686
Daniel <danielwiktorczyk@gmail.com>
2020-11-17 17:55:54 -0500

    Modify Manhattan Distance (h2)
    It is now admissible!

--- SearchAlgotrithms/heuristics.py
+++ SearchAlgotrithms/heuristics.py
@@ -18,8 +18,8 @@ def h1(board: Board) -> int:
 
 
 def h2(board: Board) -> int:
-    manhattan_distance_goal_1 = manhattan_distance(board, get_goal_1(rows=board.rows, columns=board.columns))
-    manhattan_distance_goal_2 = manhattan_distance(board, get_goal_2(rows=board.rows, columns=board.columns))
+    manhattan_distance_goal_1 = modified_manhattan_distance(board, get_goal_1(rows=board.rows, columns=board.columns))
+    manhattan_distance_goal_2 = modified_manhattan_distance(board, get_goal_2(rows=board.rows, columns=board.columns))
 
     if manhattan_distance_goal_1 <= manhattan_distance_goal_2:
         return manhattan_distance_goal_1
@@ -53,5 +53,23 @@ def manhattan_distance(board: Board, board_goal: Board) -> int:
             actual_row, actual_column = board.get_tile_position(tile)
             goal_row, goal_column = board_goal.get_tile_position(tile)
             score += abs(actual_row - goal_row) + abs(actual_column - goal_column)
+
+    return score
+
+
+# Make a variant of manhattan distance that is admissible
+# Sum up all the distances by which tiles are out of place
+def modified_manhattan_distance(board: Board, board_goal: Board) -> int:
+    score = 0
+
+    for row in range(board_goal.rows):
+        for column in range(board_goal.columns):
+            tile = board_goal.tiles[row][column]
+            actual_row, actual_column = board.get_tile_position(tile)
+            goal_row, goal_column = board_goal.get_tile_position(tile)
+            tile_score = (abs(actual_row - goal_row) + abs(actual_column - goal_column))
+            if tile_score > 2:  # I.e. the cost of a wrapping move, which invalidated the original manhattan distance
+                tile_score = 2
+            score += tile_score
 
     return score
